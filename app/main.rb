7429be868d1663b172ef91c4e00c4642034197f9
Kehvarl <Kehvarl@Kehvarl.com>
2025-05-07 20:30:07 -0700

    using menu sample for setup.  Might make a nice playfield too

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,19 @@
+require 'app/menu.rb'
+
+
 def init args
-  args.state.gamestate = :mainmenu
+  args.state.game = Menu.new()
 end
 
 def tick args
+  if args.tick_count == 0
+    init args
+  end
 
+  args.state.game.args = args
+  args.state.game.tick
+
+  if args.state.selected_button and args.inputs.mouse.click
+    puts args.state.selected_button
+  end
 end
