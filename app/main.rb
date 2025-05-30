3ff7dc398774ce224b2c7164ce735bbb6e5304a9
Kehvarl <Kehvarl@Kehvarl.com>
2025-05-29 19:37:56 -0700

    see through game-over banner

--- app/main.rb
+++ app/main.rb
@@ -144,7 +144,7 @@ def tick args
     args.state.gamestate = :game
   when :game_over
       args.outputs.primitives << args.state.game.render
-      args.outputs.primitives << {x:180,y:180,w:930,h:360,r:255,g:255,b:255}.solid!
+      args.outputs.primitives << {x:180,y:180,w:930,h:360,r:255,g:255,b:255,a:64}.solid!
       args.outputs.primitives << {x:180,y:180,w:930,h:360,r:255,g:0,b:0}.border!
       args.outputs.primitives << {x:640, y:500, text:"Game Over", size_enum: 7, anchor_x:0.5}.label!
       args.outputs.primitives << {x:640, y:400, text:"Winner: #{args.state.winner}",
